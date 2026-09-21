cdb7e90b1952e0ccfc60872cf06fb09254e2a208
at1680 <aidantt2009@gmail.com>
2026-09-21 19:15:01 +0000

    weight changes

---
+++ Weight.java
@@ -0,0 +1,11 @@
+public class Weight{
+    int pounds;
+    int ounces;
+    public Weight(int ounces){
+        this(0,ounces);
+    }
+    public Weight(int pounds, int ounces){
+        this.pounds=pounds;
+        this.ounces=ounces
+    }
+}
